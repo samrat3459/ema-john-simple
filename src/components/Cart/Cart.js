ed5f601e667d11a12ad02eff590f19f19adaae96
MD. Samrat <76749917+samrat3459@users.noreply.github.com>
2021-10-21 08:52:16 +0600

    my commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Cart = (props) => {
     const cart = props.cart;
-    // const total = cart.reduce((total, product) => total + product.price, 0)
+    
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
